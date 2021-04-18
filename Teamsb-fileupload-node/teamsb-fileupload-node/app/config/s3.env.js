40f940af004f31c377ec6738ef8ce628946e0ce0
bkp31415 <m.bharathkrishna07@gmail.com>
2021-04-18 17:59:16 +0530

    Removed some comments

--- Teamsb-fileupload-node/teamsb-fileupload-node/app/config/s3.env.js
+++ Teamsb-fileupload-node/teamsb-fileupload-node/app/config/s3.env.js
@@ -1,8 +1,8 @@
 const env = {
-	AWS_ACCESS_KEY: 'AKIAVBXSBIMNNPXM5MYT', // change to yours
-	AWS_SECRET_ACCESS_KEY: '3F6f6EPYVMPw5uiSmG46XTeBhwoRORxOVpqNvGSo', // change to yours
-	REGION : 'ap-south-1', // change to yours
-	Bucket: 'mean.fileupload' // change to yours
+	AWS_ACCESS_KEY: 'AKIAVBXSBIMNNPXM5MYT', 
+	AWS_SECRET_ACCESS_KEY: '3F6f6EPYVMPw5uiSmG46XTeBhwoRORxOVpqNvGSo',
+	REGION : 'ap-south-1', 
+	Bucket: 'mean.fileupload' 
 };
  
 module.exports = env;
