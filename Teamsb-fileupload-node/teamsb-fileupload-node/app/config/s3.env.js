361b640a0b5ff440fdc7c3d124999b0a4a6e8590
bkp31415 <m.bharathkrishna07@gmail.com>
2021-04-19 18:22:58 +0530

    deleted s3.env.js for security reasons. Will be added later for credentials using a general template

--- Teamsb-fileupload-node/teamsb-fileupload-node/app/config/s3.env.js
+++
@@ -1,8 +0,0 @@
-const env = {
-	AWS_ACCESS_KEY: 'your_acces_key', 
-	AWS_SECRET_ACCESS_KEY: 'your_secret_access_key',
-	REGION : 'your_region', 
-	Bucket: 'your_bucket' 
-};
- 
-module.exports = env;
