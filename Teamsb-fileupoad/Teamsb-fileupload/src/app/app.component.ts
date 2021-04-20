e6eec1dc2430b99ae5990fef8b3038441791b6a9
bkp31415 <m.bharathkrishna07@gmail.com>
2021-04-20 20:21:24 +0530

    Added some css and had to make some minor changes

--- Teamsb-fileupoad/Teamsb-fileupload/src/app/app.component.ts
+++ Teamsb-fileupoad/Teamsb-fileupload/src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Team SB';
-  description = 'AWS S3 File Upload with MEAN Stack.';
+  
 }
